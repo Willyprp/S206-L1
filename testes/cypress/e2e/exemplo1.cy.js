98b7ce2fa195a6f14b0963317514f5a4a3b60eba
Willyprp <willyprp@gmail.com>
2022-11-02 15:59:57 -0300

    Gerando relatorio

--- testes/cypress/e2e/exemplo1.cy.js
+++ testes/cypress/e2e/exemplo1.cy.js
@@ -2,7 +2,7 @@
 
 describe("Criando cenário de teste para o site globalsqa", () => {
 
-  it.skip("Caso de teste: Registrando um usuário no site com sucesso", () => {
+  it("Caso de teste: Registrando um usuário no site com sucesso", () => {
     cy.visit("https://globalsqa.com/angularJs-protractor/registration-login-example/#/login")
     cy.get('.btn-link').click()
     cy.get('#firstName').type("Inatel")
@@ -14,7 +14,7 @@ describe("Criando cenário de teste para o site globalsqa", () => {
 
   })
 
-  it.skip("Caso de teste: Registrando um usuário no site com falha (faltando senha)", () => {
+  it("Caso de teste: Registrando um usuário no site com falha (faltando senha)", () => {
 
     cy.visit("https://globalsqa.com/angularJs-protractor/registration-login-example/#/register")
     cy.get('#firstName').type("Inatel")
@@ -36,6 +36,15 @@ describe("Criando cenário de teste para o site globalsqa", () => {
     cy.get('#password').type(info[1])
     cy.get('.btn-primary').click()
     cy.get('h1.ng-binding').should('contain.text',info[0])
+  })
+
+  it("Caso de teste: Deletando um usuário com sucesso",()=>{
+    let info = criarUsuario()
+    cy.login(info[0],info[1])
+    cy.get('.ng-binding > a').click()
+    cy.get('.btn').click()
+    cy.login(info[0],info[1])
+    cy.get('.ng-binding').should('have.text','Username or password is incorrect')
   })
 
 
